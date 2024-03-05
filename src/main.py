44f05e4b9d9749d91f979979aa6cf1d9a4db1c74
Mukund Mauji <mauji.mukund@yahoo.ca>
2024-03-04 21:20:16 -0500

    add today/tomorrow/yesterday to date
    - also continue working on other stuff

--- src/main.py
+++ src/main.py
@@ -25,12 +25,23 @@ simple_date = seq(year=year << dash, month=month << dash, day=day).combine_dict(
     datetime.date
 )
 
+today_ = string("today").map(lambda x: datetime.date.today())
+tomorrow_ = string_from("tomorrow", "tmrw").map(
+    lambda x: datetime.date.today() + datetime.timedelta(days=1)
+)
+yesterday_ = string("yesterday").map(
+    lambda x: datetime.date.today() - datetime.timedelta(days=1)
+)
+
 
 @parsy.generate
 def date():
-    zz = simple_date.tag("simple")
+    days_ = (today_ | tomorrow_ | yesterday_).tag("days_")
+    simple_ = simple_date.tag("simple")
 
-    tag, parsed = yield zz
+    zz = yield days_ | simple_
+    tag, parsed = zz
+
     return parsed
 
 
@@ -40,17 +51,61 @@ units = parsy.string_from(
     "days", "seconds", "microseconds", "milliseconds", "minutes", "hours", "weeks"
 )
 
-how_many_until = parsy.seq(
-    prefix=string("how many days until") << parsy.whitespace, date=date
-).tag("how many days until")
-
 how_long_until = parsy.seq(
     prefix=string("how long until") << parsy.whitespace, date=date
 ).tag("how long until")
 
+how_many_until = parsy.seq(
+    prefix=string("how many days until") << parsy.whitespace, date=date
+).tag("how many days until")
+
+
+@parsy.generate
+def how_long_until_generated():
+    tag, parsed = yield (how_many_until | how_long_until)
+    start_date = datetime.date.today()
+    end_date = parsed["date"]
+
+    # instead of this assert, maybe we should fail gracefully
+    assert start_date <= end_date
+
+    delta = end_date - start_date
+
+    return {
+        "start_date": start_date,
+        "end_date": end_date,
+        "parser": tag,
+        "result": {
+            "delta": delta.days,
+        },
+    }
+
+
 how_long_since = parsy.seq(
     prefix=string("how long since") << parsy.whitespace, date=date
 ).tag("how long since")
+
+
+@parsy.generate
+def how_long_since_generated():
+    tag, parsed = yield how_long_since
+    start_date = parsed["date"]
+    end_date = datetime.date.today()
+
+    # instead of this assert, maybe we should fail gracefully
+    assert start_date <= end_date
+
+    delta = end_date - start_date
+
+    return {
+        "start_date": start_date,
+        "end_date": end_date,
+        "parser": tag,
+        "result": {
+            "delta": delta,
+        },
+    }
+
 
 time_after = parsy.seq(
     num=number << parsy.whitespace,
@@ -115,38 +170,15 @@ between = parsy.seq(prefix=between_prefix, date=date).tag("between")
 def between_generated():
     raise UnimplementedError
 
+
 def parse(target: str, override_present=None):
     tt = target.strip()
     present = datetime.date.today()
 
-    try:
-        full = time_before_generated | time_after_generated
-        return full.parse(tt)
-    except ParseError as err:
-        pass
-
-    parser_name, parsed = date_comparison_parsers.parse(tt)
-
-    # based on the parser used, we try to get the user intent
-    # and we set older and newer accordingly
-    #
-    # days_between expects older and newer passed in the right order
-    # so a failure there means we decoded the intent incorrectly
-    if parser_name == "how long since":
-        older = parsed["date"]
-        newer = present
-    elif parser_name == "how long until":
-        older = present
-        newer = parsed["date"]
-    else:
-        return {"query": target, "unknown parser": parser_name}
-
-    resp = {
-        "query": target,
-        "parser_used": parser_name,
-        "older": older.strftime("%a %d %b %Y"),
-        "newer": newer.strftime("%a %d %b %Y"),
-        "calculation": days_between(older, newer),
-    }
-
-    return resp
+    full = (
+        time_before_generated
+        | time_after_generated
+        | how_long_since_generated
+        | how_long_until_generated
+    )
+    return full.parse(tt)
